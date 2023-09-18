07c61699314b50c0ea174d2c81a0ea545866f9f9
mohan <immohanravi@gmail.com>
2023-09-18 12:27:42 +0530

    init

---
+++ app/src/main/java/com/mohanravi/composify/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.mohanravi.composify
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mohanravi.composify.ui.theme.ComposifyTheme
+import com.mohanravi.composifylib.components.CardWithAnimatedBorder
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposifyTheme {
+                // A surface container using the 'background' color from the theme
+                val colors =
+                    listOf(
+                        Color(0xFFFF595A),
+                        Color(0xFFFFC766),
+                        Color(0xFF35A07F),
+                        Color(0xFF35A07F),
+                        Color(0xFFFFC766),
+                        Color(0xFFFF595A)
+                    )
+
+                    CardWithAnimatedBorder(
+                        modifier = Modifier.padding(16.dp).fillMaxWidth().height(200.dp),
+                        borderColors = colors,
+                        shape = RoundedCornerShape(24.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "test", color = Color.Black)
+                        }
+
+                    }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposifyTheme {
+        Greeting("Android")
+    }
+}
---
+++ composifylib/src/main/java/com/mohanravi/composifylib/components/Components.kt
@@ -0,0 +1,78 @@
+package com.mohanravi.composifylib.components
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import java.util.Collections.rotate
+
+@Composable
+fun CardWithAnimatedBorder(
+    modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(20.dp),
+    borderColors: List<Color> = emptyList(),
+    onCardClick: () -> Unit = {},
+    borderRadius: Dp = 2.dp,
+    content: @Composable () -> Unit,
+) {
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val angle by
+    infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec =
+        infiniteRepeatable(
+            animation = tween(3000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
+    val brush =
+        if (borderColors.isNotEmpty()) Brush.sweepGradient(borderColors)
+        else Brush.sweepGradient(listOf(Color.Gray, Color.White))
+
+    Surface(modifier = modifier.clickable { onCardClick() }, shape = shape) {
+        Surface(
+            modifier =
+            Modifier
+                .clipToBounds()
+                .fillMaxWidth()
+                .padding(borderRadius)
+                .drawWithContent {
+                    rotate(angle) {
+                        drawCircle(
+                            brush = brush,
+                            radius = size.width,
+                            blendMode = BlendMode.SrcIn,
+                        )
+                    }
+                    drawContent()
+                },
+            //color = MaterialTheme.colorScheme.background,
+            shape = shape
+        ) {
+            Box{ content() }
+        }
+    }
+}
