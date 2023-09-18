fc25b1b7a445037f3f020cb682575a82be2bebd7
mohan <immohanravi@gmail.com>
2023-09-18 13:10:04 +0530

    added apache license

--- app/src/main/java/com/mohanravi/composify/MainActivity.kt
+++ app/src/main/java/com/mohanravi/composify/MainActivity.kt
@@ -3,7 +3,6 @@ package com.mohanravi.composify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,7 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mohanravi.composify.ui.theme.ComposifyTheme
-import com.mohanravi.composifylib.components.CardWithAnimatedBorder
+import com.mohanravi.composifylib.CardWithAnimatedBorder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposifyTheme {
                 // A surface container using the 'background' color from the theme
-                val colors =
-                    listOf(
+                val colors = listOf(
                         Color(0xFFFF595A),
                         Color(0xFFFFC766),
                         Color(0xFF35A07F),
--- composifylib/src/main/java/com/mohanravi/composifylib/Components.kt
+++ composifylib/src/main/java/com/mohanravi/composifylib/Components.kt
@@ -1,4 +1,4 @@
-package com.mohanravi.composifylib.components
+package com.mohanravi.composifylib
 
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,8 +24,14 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import java.util.Collections.rotate
 
+
+/**
+ *  This is mohan created this composable by the reading the blog tutorial, link is given below
+ * @author Ruthwik
+ * @author https://ruthwikkk.medium.com/animate-borders-in-jetpack-compose-ca359deed7d5
+ * @modified by Mohan Ravi
+ */
 @Composable
 fun CardWithAnimatedBorder(
     modifier: Modifier = Modifier,
